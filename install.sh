4199767373b08ef9086a1b1431be7868c5ae2f82
Sam Bennett <sbennett@potomacschool.org>
2020-11-01 13:07:14 -0500

    added installer and component tester

---
+++ install.sh
@@ -0,0 +1,33 @@
+:'
+    description
+    -----------
+    - install script for the Ent project and all of its dependencies
+
+    instructions
+    ------------
+    - run as root with the following command: sudo bash install.sh
+'
+
+# install git
+apt-get install git
+
+# install python dependencies
+pip3 install --pre azure-cosmos
+pip3 install wiringpi
+pip3 install darksky_weather
+
+# make download directory
+mkdir ent
+cd ent
+$path = pwd
+
+# install megaio dependencies
+git clone https://github.com/SequentMicrosystems/megaio-rpi.git 
+cd ./megaio-rpi
+sudo make install 
+cd ./python/megaio
+python3 setup.py install
+
+# install ent
+cd $path
+git clone https://github.com/sambennett04/Ent.git
